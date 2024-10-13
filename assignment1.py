73f970addf6976e8b110c0ed63c18176c14827c3
ishanj33 <ishan.jain@csuglobal.edu>
2024-10-13 16:41:24 -0700

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,28 @@
+def add_nums():
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) + int(num2))
+
+def subtract_nums():
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) - int(num2))
+
+def multiply_nums():
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) * int(num2))
+
+def divide_nums():
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) / int(num2))
+
+def main():
+    add_nums()
+    subtract_nums()
+    multiply_nums()
+    divide_nums()
+
+if __name__== "__main__":
+    main()
